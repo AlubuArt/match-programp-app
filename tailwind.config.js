60c139d5ea9dd9df8ba2360a3f5f93a2c0b7ab53
AlubuArt <jc@jcvisueldesign.dk>
2022-01-15 08:41:13 +0100

    styling header

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,7 @@ module.exports = {
   theme: {
     extend: {
       fontFamily: {
-        'sans': ['Helvetica', 'Arial', 'sans-serif'],
+        'sans': ['Montserrat', 'Arial', 'sans-serif'],
         
       },
       colors: {
--- ui/components/Layouts/ProgramLayout/ProgramLayout.tsx
+++ ui/components/Layouts/ProgramLayout/ProgramLayout.tsx
@@ -8,11 +8,18 @@ interface ProgramLayoutProps {
 export const ProgramLayout: React.FC<ProgramLayoutProps> = ({ matches }) => {
     return (
         <div className="container mx-auto">
-            <div className="container pt-10  w-4/5 mx-auto text-center font-sans text-white">
-                <h1 className="text-5xl font-bold">Match Day!</h1>
-                <h2 className="text-3xl font-medium"> Lørdag d. 13/3</h2>
+            <div className="container pt-10  w-5/5 mx-auto text-white">
+                <div className="text-center pr-5 pl-5">
+                    <h1 className="text-6xl font-black italic">KAMPDAG!</h1>
+                </div>
+                <div className="text-right pr-5 pl-5">
+                    <h2 className="text-1xl font-bold italic">
+                        Lørdag d. 13/3
+                    </h2>
+                </div>
             </div>
-            <div className="container py-20 w-3/5  mx-auto">
+            <div></div>
+            <div className="container py-20 w-4/5  mx-auto">
                 {matches.map((match, index) => {
                     return <MatchCard match={match} key={index} />;
                 })}
