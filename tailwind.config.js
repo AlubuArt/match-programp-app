9dedcdf47855ee792520bc1e7e8e9fd07c29aa02
AlubuArt <jc@jcvisueldesign.dk>
2022-01-15 15:47:00 +0100

    changed folder

--- tailwind.config.js
+++ tailwind.config.js
@@ -15,9 +15,9 @@ module.exports = {
         'white': '#FFFFFF',
       },
       backgroundImage: {
-        'body-image': "url('../public/images/body-image.jpeg')",
+        'body-image': "url('../public/body-image.jpeg')",
         'customGradient': "linear-gradient(to bottom, var(blue), var(dark-blue)",
-        'noise': "url('../public/images/Noise.png",
+        'noise': "url('../public/Noise.png",
         'card-gradient': "linear-gradient(to right, rgba(255, 255, 255, 0.22), rgba(255, 255, 255, 0.09) 100%)"
       },
     },
