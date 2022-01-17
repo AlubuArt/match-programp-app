ec631bcd39ad0b52ad8f103a2fdddc42ed4fee05
AlubuArt <jc@jcvisueldesign.dk>
2022-01-17 20:28:07 +0100

    added tracking from GA

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,14 +1,20 @@
 import type { GetServerSideProps, GetStaticProps, NextPage } from "next";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { getMatchPrograms } from "../lib/graphcms";
 import Footer from "../ui/components/Footer/Footer";
 import Header from "../ui/components/Header/Header";
 import ProgramLayout from "../ui/components/Layouts/ProgramLayout/ProgramLayout";
 import Pagnation from "../ui/components/Pagnation/Pagnation";
+import TagManager from 'react-gtm-module';
 
 const Home: NextPage = (program: any) => {
 
     let [selectedRound, setSelectedRound] = useState(0);
+
+    useEffect(() => {
+        TagManager.initialize({ gtmId: 'GTM-WF57VTL' });
+       
+    }, [])
 
     return (
         <div className="mx-auto bg-body-image min-h-screen relative">
